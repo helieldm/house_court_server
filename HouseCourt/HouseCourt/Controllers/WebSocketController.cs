b846ed1cd9339ad2de709b5bd01d4efe13713d29
Bastien Brunet <b.brunet@itso.fr>
2022-12-02 14:53:01 +0100

    Init project and websocket

---
+++ HouseCourt/HouseCourt/Controllers/WebSocketController.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+
+namespace HouseCourt.Controllers;
+using System.Net.WebSockets;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("[controller]")]
+public class WebSocketController : ControllerBase
+{
+    private readonly ILogger<WebSocketController> _logger;
+
+    public WebSocketController(ILogger<WebSocketController> logger)
+    {
+        _logger = logger;
+    }
+    
+    [HttpGet("/ws")]
+    public async Task Get()
+    {
+        if (HttpContext.WebSockets.IsWebSocketRequest)
+        {
+            using var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+            await Echo(webSocket);
+        }
+        else
+        {
+            HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+        }
+    }
+    // </snippet>
+
+    private async Task Echo(WebSocket webSocket)
+    {
+        var buffer = new byte[1024 * 4];
+        var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+        _logger.Log(LogLevel.Information, "Message received from Client");
+
+        while (!result.CloseStatus.HasValue)
+        {
+            var serverMsg = Encoding.UTF8.GetBytes($"Server: Hello. You said: {Encoding.UTF8.GetString(buffer)}");
+            await webSocket.SendAsync(new ArraySegment<byte>(serverMsg, 0, serverMsg.Length), result.MessageType, result.EndOfMessage, CancellationToken.None);
+            _logger.Log(LogLevel.Information, "Message sent to Client");
+
+            result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            _logger.Log(LogLevel.Information, "Message received from Client");
+                
+        }
+        await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+        _logger.Log(LogLevel.Information, "WebSocket connection closed");
+    }
+}
